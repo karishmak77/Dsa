eb452e693c9de569b41760e979c7f9ac6f4f23da
Karishma kanhar <149614986+karishmak77@users.noreply.github.com>
2025-07-29 09:37:53 +0530

    missVaule.java

--- Array/missVaule.java
+++ Array/missVaule.java
@@ -1,3 +1,5 @@
+// Quetion : Find Missing Values Between Consecutive Elements in a Sorted Array   [ level : easy ]
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,3 +31,25 @@ public class missVaule {
  }
   
 }
+
+
+
+
+// ➤ missingVaule(arr)
+// This is the main function that receives the input list and prints the result.
+// It calls the helper function values(arr) to get the missing numbers.
+// ➤ values(arr)
+// Iterates through the list, comparing each element (current) with the next (next).
+// For each pair, it checks if there are numbers between them:
+// Example: if current = 2, next = 5 → it adds 3, 4 to the result.
+// All missing numbers between each consecutive pair are added to a new list missValue.
+
+
+// Time and Space Complexity:
+// Time Complexity: O(m + n)
+// n = number of elements in the list
+// m = number of missing values between elements
+// Because we traverse both original and missing values (depending on the range), total time is proportional to both
+// Space Complexity: O(m)
+// A new list missValue is used to store the missing numbers
+
