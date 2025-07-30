dad3378f75dc4ee6b87c17bcbd1466e35e67c0fb
Karishma kanhar <149614986+karishmak77@users.noreply.github.com>
2025-07-30 11:45:21 +0530

    Add files via upload

---
+++ Array/LeftRotate.java
@@ -0,0 +1,37 @@
+// Question : Left Rotate the Array by One [level: easy]
+public class LeftRotate {
+  public static void InnerLeftRotate(int arr[]) {
+    if (arr.length==0) {
+      System.out.println("array is empty");
+      return;
+    }
+
+    int first= arr[0];
+    for(int i=0;i<arr.length-1;i++){
+      arr[i]=arr[i+1];
+    }
+
+    arr[arr.length-1] =first;
+
+    System.out.println("after Left Rotation by One: ");
+    for(int j=0;j<arr.length;j++){
+      System.out.print(arr[j]+" ");
+    }
+  
+    
+  }
+
+  public static void main(String[] args) {
+    int arr[] = {1,2,3,4,5};
+    System.out.println("Current arrray");
+     for(int j=0;j<arr.length;j++){
+      System.out.println(arr[j]+" ");
+    }
+
+    InnerLeftRotate(arr);
+
+
+  }
+
+  
+}
